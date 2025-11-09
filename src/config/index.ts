748f35c526ef108d9c2cba41a4140cceb0b86a42
jahangirWenexus <jahangir@wenexus.io>
2025-11-09 10:17:06 +0600

    Changed based url

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,4 @@
 // export const BASE_URL =
 //   " https://journal-julie-breakdown-strips.trycloudflare.com";
 
-export const BASE_URL = "https://crops-this-operations-snowboard.trycloudflare.com" //"https://sales-discounts.wenexus.io";
+export const BASE_URL = "https://sales-discounts.wenexus.io";
